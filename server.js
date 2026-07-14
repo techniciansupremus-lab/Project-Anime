d5a78dc1bb2f18f29141d064260e5d7886ad24b1
Jellisous <rajeshlamu45@gmail.com>
2026-07-14 21:51:33 +0545

    Trust proxy in Express to resolve mixed content HLS blocks on mobile

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('trust proxy', true);
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: '*' }));
